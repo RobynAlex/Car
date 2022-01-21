bac7c24f9286ed167b580595f6da48be70da5883
RobynAlex <robyn.alexander1111@gmail.com>
2022-01-20 18:25:40 -0600

    Add project files.

---
+++ Car/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car
+{
+    public class Car
+    {
+        internal int amount;
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public string Color { get; set; }
+
+
+
+        
+
+
+
+
+
+    }
+}
---
+++ Car/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Car
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vehicle = new Car();
+
+            vehicle.Make = "Dodge";
+            vehicle.Model = "Caliber";
+            vehicle.Year = 2007;
+            vehicle.Color = "Black";
+
+            Console.WriteLine($"There are 4 {vehicle.Make} {vehicle.Model}s in the area.");
+
+        }
+    }
+}
